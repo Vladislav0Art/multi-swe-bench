453d81eb6e7a13c52211324931e02e15b13f7b88
Vladislav Artiukhov <vladislav.artiukhov@jetbrains.com>
2026-04-16 02:59:24 +0200

    fix: fix import to resolve circular dep to metamorphic.py file

--- multi_swe_bench/harness/repos/java/fasterxml/jackson_databind_4587_to_4418.py
+++ multi_swe_bench/harness/repos/java/fasterxml/jackson_databind_4587_to_4418.py
@@ -2,7 +2,7 @@ import re
 import json
 from typing import Optional, Union
 
-from multi_swe_bench.harness import Metamorphic
+from multi_swe_bench.harness.metamorphic import Metamorphic
 from multi_swe_bench.harness.image import Config, File, Image
 from multi_swe_bench.harness.instance import Instance, TestResult
 from multi_swe_bench.harness.pull_request import PullRequest
